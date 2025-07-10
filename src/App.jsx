1dd11428fccd6e4f55f317fa95d74f981c6a2d4d
yeon-yeon1 <nojk0106@naver.com>
2025-07-11 06:24:20 +0900

    ✅ 배포 설정

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Profile from "./pages/Profile";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    cursor: url(https://cur.cursors-4u.net/food/foo-6/foo507.ani),
+            url(https://cur.cursors-4u.net/food/foo-6/foo507.png),
+            auto !important;
+  
+  margin: 0;
+  padding: 0;
+  }
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+export default App;
